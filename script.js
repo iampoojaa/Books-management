245b61cbc7fb70b327b03419e0d685ef825d7a6a
iampooja.io <poojakv2203@gmail.com>
2021-06-19 20:31:27 +0530

    Complete projrct

---
+++ script.js
@@ -0,0 +1,20 @@
+$('#year').text(new Date().getFullYear());
+
+// smooth scrolling
+$("#main-nav a").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        
+        const hash = this.hash;
+  
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        500,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
